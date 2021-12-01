51e48750a74ef009a41b9c2b3154e81a61436e91
qinxinghua <qinxinghua@lexue.com>
2021-12-01 10:58:55 +0800

    增加命令行项目

---
+++ ALL_Mac_Prac/ALL_Mac_Prac/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ALL_Mac_Prac
+//
+//  Created by qinxinghua on 2021/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
